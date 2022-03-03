04c3403e98ff50e8d3d3fd3febfbbf0be26f6dc1
Muhammad Asharul Maali <masharul51@gmail.com>
2022-03-03 20:20:52 +0700

    Add files via upload

---
+++ Studi Independen/Pertemuan 13 DOM/js/switch.js
@@ -0,0 +1,12 @@
+switch(prd){
+    case 'Televisi':
+        harga = 2000000;
+        break;
+    case 'Air Conditioner':
+        harga = 3000000;
+        break;
+    case 'Kulkas':
+        harga = 4000000;
+        break;
+    default :'Barang tidak ditemukan';
+}
